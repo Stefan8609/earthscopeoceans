b9358a331cf04698522bd4937961aab8503b4ec6
Stefan Kildal-Brandt <99145660+Groutesqueagate@users.noreply.github.com>
2022-06-29 11:59:18 -0400

    Delete BioPopup.js
    Unused file

--- scripts/BioPopup.js
+++
@@ -1,9 +0,0 @@
-let openBios = [];
-
-// When the user clicks on div, open the popup
-function myFunction(id) {
-
-	let popup = document.getElementById(id);
-	popup.classList.toggle("show");
-	openBios.push(popup);
-}
